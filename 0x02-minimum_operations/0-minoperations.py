566404ba6498004a3bc63e3d01658f3d328e8078
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-08 17:50:50 +0200

    interview week 3

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""copy and paste operations."""
+
+
+def minOperations(n: int) -> int:
+    """a function to return the minimum number of copy past operations.
+       Args:
+           n(int): number of operations to be computed
+    """
+    if n <= 1:
+        return 0
+    operations: int = 0
+    factor: int = 2
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
