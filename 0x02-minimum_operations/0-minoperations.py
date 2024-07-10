b0cee0fcf9d5a6875bb2f0094c1fb990148fcade
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-10 16:42:38 +0200

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,15 +2,15 @@
 """copy and paste operations."""
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     """a function to return the minimum number of copy past operations.
        Args:
            n(int): number of operations to be computed
     """
     if n <= 1:
         return 0
-    operations: int = 0
-    factor: int = 2
+    operations = 0
+    factor = 2
     while n > 1:
         while n % factor == 0:
             operations += factor
