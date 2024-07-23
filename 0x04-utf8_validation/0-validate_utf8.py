ee616ad6374a1adef7bc20af79f48ede9cfa1961
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-23 12:31:24 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,19 +4,28 @@
 
 def validUTF8(data):
     """method that determines if data set represents a valid UTF-8 encoding"""
-    num_bytes = 0
-    for byte in data:
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
+    def get_num_bytes(byte):
+        """Check the number of leading 1s in the first byte"""
+        if (byte >> 5) == 0b110:
+            return 2
+        elif (byte >> 4) == 0b1110:
+            return 3
+        elif (byte >> 3) == 0b11110:
+            return 4
+        elif (byte >> 7) == 0:
+            return 1
+        return -1
+
+    i = 0
+    while i < len(data):
+        num_bytes = get_num_bytes(data[i])
+        if num_bytes == -1 or i + num_bytes > len(data):
+            return False
+
+        for j in range(1, num_bytes):
+            if (data[i + j] >> 6) != 0b10:
                 return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            num_bytes -= 1
-    return num_bytes == 0
+
+        i += num_bytes
+
+    return True
