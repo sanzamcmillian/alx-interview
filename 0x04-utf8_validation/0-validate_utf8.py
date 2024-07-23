7db55c61b1cb671758847e80702a440c1379cce5
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-23 12:14:07 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,19 @@
 
 def validUTF8(data):
     """method that determines if data set represents a valid UTF-8 encoding"""
-    for i in data:
-        if i > 0 and i <= 128:
-            return True
-        return False
+    num_bytes = 0
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
