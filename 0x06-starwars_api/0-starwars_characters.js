b616a77048a71ab870498963ed5dde797285a406
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-08 13:15:04 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,7 +8,7 @@ request('https://swapi-api.alx-tools.com' + process.argv[2], function (error, re
   exactOrder(actors, 0);
 });
 const exactOrder = (actors, x) => {
-  if (actors.length === x) return;
+  if (x === actors.length) return;
   request(actors[x], function (error, response, body) {
     if (error) throw error;
     console.log(JSON.parse(body).name);
