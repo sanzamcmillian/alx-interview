e841bc2bbc8cc83be8f2e6ec831c7439f79c0fc1
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-15 19:36:20 +0200

    final push

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,6 +5,7 @@
 def rotate_2d_matrix(matrix):
     """A function to implement 2-D matrix rotation"""
     n = len(matrix)
+    matrix.reverse()
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
