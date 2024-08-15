adb9bf29ac663be7e4fb63412b85d70a2bea25b3
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-15 10:11:18 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """module to rotate a 2-D matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """A function to implement 2-D matrix rotation"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for row in matrix:
+        matrix.reverse()
