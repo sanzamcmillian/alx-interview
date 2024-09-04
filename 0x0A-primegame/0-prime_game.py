aa920a29f8cd5895d98abc6fbb2af3d565c48cf5
sanzamcmillian <g18s2658@ru.ac.za>
2024-09-04 10:28:57 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,9 +6,11 @@ def prime_list(n):
     """method generate a prime number list"""
     if n <= 1:
         return False
+
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
+
     return True
 
 
@@ -17,19 +19,24 @@ def isWinner(x, nums):
     p_list = []
     ben_score = 0
     maria_score = 0
+
     for i in range(x):
+
         n = list(range(1, nums[i] + 1))
+
         for j in range(len(n)):
             if (prime_list(n[j]) is not True):
                 continue
             else:
                 p_list.append(n[j])
+
         if len(p_list) % 2 == 0:
             ben_score += 1
             p_list.clear()
         else:
             maria_score += 1
             p_list.clear()
+
     if ben_score > maria_score:
         return 'Ben'
     elif maria_score > ben_score:
